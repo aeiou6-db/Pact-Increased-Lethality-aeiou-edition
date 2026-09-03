fa2653589537eb8d374b16bc754042abe6f9828f
aeiou6-db <danielbazzi4@gmail.com>
2026-09-03 18:37:36 -0500

    Fixed round order on the random ammo pool
    Co-authored-by: Benjamin Nguyen <89621837+thebeninator@users.noreply.github.com>

--- Vics/BTR60.cs
+++ Vics/BTR60.cs
@@ -113,7 +113,7 @@ namespace PactIncreasedLethality
             btr_AP_round.Comment = "3UBR6, 3UBR8, 3UBR11";
             btr_random_ammo = cfg.CreateEntry<bool>("Random AP Round (BTR-60A)", false);
             btr_random_ammo_pool = cfg.CreateEntry<List<string>>("Random AP Round Pool (BTR-60A)", random_ammo_pool);
-            btr_random_ammo_pool.Comment = "3UBR8, 3UBR6, 3UBR11";
+            btr_random_ammo_pool.Comment = "3UBR6, 3UBR8, 3UBR11";
             use_3uof8 = cfg.CreateEntry<bool>("Use 3UOF8 (BTR-60A)", false);
             use_3uof8.Comment = "Mixed belt of 3UOR6 and 3UOF8 (1:2); 3UOF8 has more explosive filler but no tracer";
         }
